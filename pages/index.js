2a854b00fcbd0285ab90858c024e95025ec3eabc
Guilherme de Oliveira <54085382+gdodev@users.noreply.github.com>
2025-11-23 02:13:30 +0000

    Atualiza conteudo da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste</h1>;
+    return <h1>Olá meu caro! Saiba que o senhor é uma pessoa ímpar. Um ser humano muito educado, simpático e é um prazer trabalhar ao seu lado todos os dias. Um forte abraço!</h1>;
 }
 
 export default Home;
